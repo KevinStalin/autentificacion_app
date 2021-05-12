d7691925ec2d7b156448364087e095844ec80232
Kevin_Ramirez <stalram98@gmail.com>
2021-05-12 14:13:53 -0500

    Validaciones (logup) 15 campos 85% completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-// import { LoginComponent } from './views/login/login.component';
-// import { LogupComponent } from './views/logup/logup.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
-import { ErrorTailorModule } from '@ngneat/error-tailor';
 import { LoginAuntentComponent } from './views/login-auntent/login-auntent.component';
 import { LogupAutentComponent } from './views/logup-autent/logup-autent.component';
 
@@ -20,8 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    // LoginComponent,
-    // LogupComponent,
+
     routingComponents,
     LoginAuntentComponent,
     LogupAutentComponent
@@ -31,17 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    ErrorTailorModule.forRoot({
-      errors: {
-        useValue: {
-          required: 'campo requerido',
-          minlength: ({ requiredLength, actualLength }) =>
-            `Expect ${requiredLength} but got ${actualLength}`,
-          invalidAddress: error => `Address isn't valid`
-        }
-      }
-    }),
-    HttpClientModule
+    HttpClientModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
